2ec77446b58a2ea19e9c365014f5e42d42e86aaa
teddy-hoo <hulingchuan@hotmail.com>
2014-10-27 23:05:22 +0800

    function complete, but low performance

--- orienteering.cpp
+++ orienteering.cpp
@@ -59,7 +59,6 @@ public:
 			cout << -1 << endl;
 		}
 
-		buildGraph();
 		cout << findPath() << endl;
 
 	}
@@ -67,53 +66,148 @@ public:
 private:
 
 	int findPath(){
-		int max = -1;
+		int flag[100][100];
 
-		stack<Node *> sn;
-		vector<int> si;
-		sn.push(graph[0]);
-		si.push_back(0);
-
-		int path = 0;
-		while(!sn.empty()){
-			if(si.size() == graph.size()){
-				int top = si.back();
-				if(graph[top]->flag == GOAL){
-					max = path > max ? path : max;
-				}
-				break;
-				si.pop_back();
-				sn.pop();
-			}
-			Node * cur = sn.top();
-			Node * next = cur->next;
-
-			while(next != NULL){
-				bool has = false;
-				for(int i = 0; i < si.size(); i++){
-					if(si[i] == next->index){
-						has = true;
-					}
-				}
-				if(has == false){
-					break;
-				}
-				next = next->next;
-			}
-			if(next == NULL){
-				sn.pop();
-				si.pop_back();
-			}
-
-			else{
-				path += next->cost;
-				sn.pop();
-				sn.push(next);
-				si.push_back(next->index);
-				sn.push(graph[next->index]);
+		for(int i = 0; i < height; i++){
+			for(int j = 0; j < width; j++){
+				flag[i][j] = 0;
 			}
 		}
-		return max;
+
+		int steps = 0;
+		int count = 0;
+		int min = 100000;
+
+		stack<int> s1;
+		stack<int> s2;
+		stack<int> s3;
+		s1.push(startPoint[0]);
+		s2.push(startPoint[1]);
+		s3.push(0);
+		while(!s1.empty()){
+			int x = s1.top();
+			int y = s2.top();
+			int d = s3.top();
+			s3.pop();
+	
+			d++;
+			if(d == 1){
+				if( x - 1 >= 0 &&
+				    flag[x - 1][y] < 2 &&
+				    metrix[x - 1][y] != '#'){
+					if(metrix[x - 1][y] == 'G'){
+						if(count == checkPointCount){
+							steps++;
+							min = min > steps ? steps : min;
+							steps--;
+						}
+					}
+					else if(metrix[x - 1][y] == '@' && flag[x - 1][y] < 1){
+						count++;
+					}
+					steps++;
+					flag[x - 1][y]++;
+					s3.push(d);
+					s1.push(x - 1);
+					s2.push(y);
+					s3.push(0);
+				}
+				else{
+					s3.push(d);
+				}
+			}
+			else if(d == 2){
+				if( x + 1 < width &&
+				    flag[x][y + 1] < 2 &&
+				    metrix[x][y + 1] != '#'){
+					if(metrix[x][y + 1] == 'G'){
+						if(count == checkPointCount){
+							steps++;
+							min = min > steps ? steps : min;
+							steps--;
+						}
+					}
+					else if(metrix[x][y + 1] == '@' && flag[x][y + 1] < 1){
+						count++;
+					}
+					steps++;
+					flag[x][y + 1]++;
+					s3.push(d);
+					s1.push(x);
+					s2.push(y + 1);
+					s3.push(0);
+				}
+				else{
+					s3.push(d);
+				}
+			}
+			else if(d == 3){
+				if( x + 1 < height &&
+				    flag[x + 1][y] < 2 &&
+				    metrix[x + 1][y] != '#'){
+					if(metrix[x + 1][y] == 'G'){
+						if(count == checkPointCount){
+							steps++;
+							min = min > steps ? steps : min;
+							steps--;
+						}
+					}
+					else if(metrix[x + 1][y] == '@' && flag[x + 1][y] < 1){
+						count++;
+					}
+					steps++;
+					flag[x + 1][y]++;
+					s3.push(d);
+					s1.push(x + 1);
+					s2.push(y);
+					s3.push(0);;
+				}
+				else{
+					s3.push(d);
+				}
+			}
+			else if(d == 4){
+				if( y - 1 >= 0 &&
+				    flag[x][y - 1] < 2 &&
+				    metrix[x][y - 1] != '#'){
+					if(metrix[x][y - 1] == 'G'){
+						if(count == checkPointCount){
+							steps++;
+							min = min > steps ? steps : min;
+							steps--;
+						}
+					}
+					else if(metrix[x][y - 1] == '@' && flag[x][y - 1] < 1){
+						count++;
+					}
+					steps++;
+					flag[x][y - 1]++;
+					s3.push(d);
+					s1.push(x);
+					s2.push(y - 1);
+					s3.push(0);
+				}
+				else{
+					flag[x][y]--;
+					steps--;
+					if(metrix[x][y] == '@' && flag[x][y] == 0){
+						count--;
+					}
+					s1.pop();
+					s2.pop();
+				}
+			}
+			else{
+				flag[x][y]--;
+				steps--;
+				if(metrix[x][y] == '@' && flag[x][y] == 0){
+					count--;
+				}
+				s1.pop();
+				s2.pop();
+			}
+		}
+		return min;
 	}
 
 	void readData(){
@@ -156,181 +250,15 @@ private:
 		metrix.push_back(row);
 	}
 
-	void buildGraph(){
-
-		int count = 0;
-
-		Node *s = new Node(START, 0, -1, NULL, startPoint);
-		graph.push_back(s);
-
-		while( count < checkPointCount + 2 ){
-			int row = graph[count]->pos[0];
-			int col = graph[count]->pos[1];
-
-			vector<Node *> neighbors = findNeighbor(row, col);
-
-			Node* curNode = graph[count];
-			for( int i = 0; i < neighbors.size(); i++){
-				Node * n = neighbors[i];
-				int index = notExists(graph, n->pos[0], n->pos[1]);
-				if(index < 0){
-					graph.push_back(n);
-					n->index = graph.size() - 1;
-				}
-				else{
-					n->index = index;
-				}
-				Node * another = graph[n->index];
-				while(another->next != NULL){
-					another = another->next;
-				}
-				another->next = graph[count];
-				curNode->next = n;
-				curNode = n;
-			}
-			curNode->next = NULL;
-
-			metrix[row][col] = '#';
-			count++;
-		}
-	}
-
-	int notExists(vector<Node *> &nodes, int r, int c){
-		for(int i = 0; i < nodes.size(); i++){
-			if(nodes[i]->pos[0] == r && nodes[i]->pos[1] == c){
-				return i;
-			}
-		}
-		return -1;
-	}
-
-	void addNeighbor(vector<Node *> &neighbors, int r, int c, int cost){
-		if(notExists(neighbors, r, c) < 0){
-			int pos[2];
-			pos[0] = r;
-			pos[1] = c;
-			Node * e = new Node(metrix[r][c], -1, cost, NULL, pos);
-			neighbors.push_back(e);
-		}
-	}
-
-	vector<Node *> findNeighbor(int row, int col){
-		vector<Node *> neighbors;
-
-		int top = false, left = false, right = false, bottom = false;
-		// form four direction to find neighbors, top left right bottom
-		
-		int r = row - 1; //top
-		int c = col;
-		while(metrix[r][c] == OPENBLOCK && r >= 0){ 
-
-			int mc = c - 1;
-			while(metrix[r][mc] == OPENBLOCK && mc >= 0){ //top left
-				mc --;
-			}
-			if(mc >= 0 && metrix[r][mc] == CHECKPOINT || metrix[r][mc] == GOAL){
-				addNeighbor(neighbors, r, mc, r + mc - row - col);
-			}
-
-			mc = c + 1;
-			while(metrix[r][mc] == OPENBLOCK && mc < width){ //top right
-				mc ++;
-			}
-			if(mc < width && metrix[r][mc] == CHECKPOINT || metrix[r][mc] == GOAL){
-				addNeighbor(neighbors, r, mc, r + mc - row - col);
-			}
-
-			r --;
-		}
-		if(r >= 0 && metrix[r][c] == CHECKPOINT || metrix[r][c] == GOAL){
-			addNeighbor(neighbors, r, c, r + c - row - col);
-		}
-
-		r = row; //left
-		c = col - 1;
-		while(metrix[r][c] == OPENBLOCK && c >= 0){ 
-
-			int mr = r - 1;
-			while(metrix[mr][c] == OPENBLOCK && mr >= 0){ //left top
-				mr --;
-			}
-			if(mr >= 0 && metrix[mr][c] == CHECKPOINT || metrix[mr][c] == GOAL){
-				addNeighbor(neighbors, mr, c, mr + c - row - col);
-			}
-
-			mr = r + 1;
-			while(metrix[mr][c] == OPENBLOCK && mr < height){ //left bottom
-				mr ++;
-			}
-			if(mr < height && metrix[mr][c] == CHECKPOINT || metrix[mr][c] == GOAL){
-				addNeighbor(neighbors, mr, c, mr + c - row - col);
-			}
-			c--;
-		}
-		if(c >= 0 && metrix[r][c] == CHECKPOINT || metrix[r][c] == GOAL){
-			addNeighbor(neighbors, r, c, r + c - row - col);
-		}
-
-		r = row; //right
-		c = col + 1;
-		while(metrix[r][c] == OPENBLOCK && c < width){ 
-
-			int mr = r - 1;
-			while(metrix[mr][c] == OPENBLOCK && mr >= 0){ //right top
-				mr --;
-			}
-			if(mr >= 0 && metrix[mr][c] == CHECKPOINT || metrix[mr][c] == GOAL){
-				addNeighbor(neighbors, mr, c, mr + c - row - col);
-			}
-
-			mr = r + 1;
-			while(metrix[mr][c] == OPENBLOCK && mr < height){ //top right
-				mr ++;
-			}
-			if(mr < height && metrix[mr][c] == CHECKPOINT || metrix[mr][c] == GOAL){
-				addNeighbor(neighbors, mr, c, mr + c - row - col);
-			}
-			c++;
-
-		}
-		if(c < width && metrix[r][c] == CHECKPOINT || metrix[r][c] == GOAL){
-			addNeighbor(neighbors, r, c, r + c - row - col);
-		}
-
-		r = row + 1; //
-		c = col;
-		while(metrix[r][c] == OPENBLOCK && r < height){ 
-
-			int mc = c - 1;
-			while(metrix[r][mc] == OPENBLOCK && mc >= 0){ //bottom left
-				mc --;
-			}
-			if(mc >= 0 && metrix[r][mc] == CHECKPOINT || metrix[r][mc] == GOAL){
-				addNeighbor(neighbors, r, mc, r + mc - row - col);
-			}
-
-			mc = c + 1;
-			while(metrix[r][mc] == OPENBLOCK && mc < width){ //bottom right
-				mc ++;
-			}
-			if(mc < width && metrix[r][mc] == CHECKPOINT || metrix[r][mc] == GOAL){
-				addNeighbor(neighbors, r, mc, r + mc - row - col);
-			}
-			r ++;
-		}
-		if(r < height && metrix[r][c] == CHECKPOINT || metrix[r][c] == GOAL){
-			addNeighbor(neighbors, r, c, r + c - row - col);
-		}
-
-		return neighbors;
-	}
 };
 
-int main(int argc, char* argv[]){
+
+int main()
+{
 
 	Orienteering o;
 
 	o.main();
-
 	return 0;
 }
+
